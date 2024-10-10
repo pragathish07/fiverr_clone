f3857acf11bb9b5f2a4843cc283369f5f9e8c328
pragathish07 <pragarajesh779jd@gmail.com>
2024-10-10 18:19:12 +0530

    dockerized postgres

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/AuthRoutes.js";
+import cookieParser from "cookie-parser";
+import { gigRoutes } from "./routes/GigRoutes.js";
+import { orderRoutes } from "./routes/OrderRoutes.js";
+import { messageRoutes } from "./routes/MessageRoutes.js";
+import { dashboardRoutes } from "./routes/DashboardRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+const allowedOrigins = [process.env.ORIGIN, 'http://localhost:3000'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use("/uploads", express.static("uploads"));
+app.use("/uploads/profiles", express.static("uploads/profiles"));
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/gigs", gigRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
